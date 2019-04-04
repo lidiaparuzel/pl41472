5e464e4ad96ca77bed66189f5534c13d7b5a7e27
Snowe <areka7270@wp.pl>
2019-04-04 12:16:15 +0200

    zajecia 04.04.2019

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/*
+poprzednie zajecia
 const test = (ev, a) => {
     console.log ("Log");
     console.log (ev);
@@ -9,3 +11,25 @@ const double = (a, b) => a*b;
 window.addEventListener('DOMContentLoaded', x => {
     test(x, 5);
 });
+*/
+
+
+
+const init = () => {
+    const tab = ["Display",7,8,9,"C",4,5,6,"+",1,2,3,"-",0,".","="];
+    const getData = () => {
+        console.log("jestem!!");
+    }
+    var main = document.getElementById("container");
+    tab.forEach((el,id) => {
+        var item=document.createElement("div");
+        if (el=="Display") item.className="resultDisplay"
+        else if (el==0) item.className="buttonBig";
+        else item.className="buttonSmall";
+        item.textContent=el;
+        item.addEventListener("click", getData);
+        main.appendChild(item);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', init);
