7d49f701ec9fdb229cb8f3fc3a42031ef028b91a
Snowe <areka7270@wp.pl>
2019-04-11 12:34:22 +0200

    zajecia 11.04.2019

--- main.js
+++ main.js
@@ -16,18 +16,59 @@ window.addEventListener('DOMContentLoaded', x => {
 
 
 const init = () => {
-    const tab = ["Display",7,8,9,"C",4,5,6,"+",1,2,3,"-",0,".","="];
-    const getData = () => {
-        console.log("jestem!!");
+    const tab = ["",7,8,9,"C",4,5,6,"+",1,2,3,"-",0,".","="];
+    const nums = ["1","2","3","4","5","6","7","8","9","0","."];
+    const operations = ["+","-"];
+    let op = "";
+    let przecinek = 0;
+    let result=0;
+    let clear=0;
+    let first=1;
+    let temp=0;
+    const calculator = (x) => {
+        button = document.getElementsByClassName("resultDisplay");
+        if (operations.includes(x.target.textContent) || (x.target.textContent == "C")) {przecinek=0;result=0}
+        if (nums.includes(x.target.textContent)){
+            if(clear==1){button[0].textContent ="";clear=0}
+            if ((x.target.textContent == ".") && (przecinek == 0)) {
+                button[0].textContent =  button[0].textContent + x.target.textContent;
+                przecinek++;
+            }
+            else if ((x.target.textContent == ".") && (przecinek != 0)) {}
+            else button[0].textContent =  button[0].textContent + x.target.textContent;
+        }
+        if (first==1) {
+            temp = button[0].textContent;
+            if (x.target.textContent == "-") {result=temp-parseFloat(button[0].textContent);button[0].textContent=result;clear=1;op="-";first=0}
+        } else {
+            if (x.target.textContent == "-") {result=temp-parseFloat(button[0].textContent);button[0].textContent=result;clear=1;op="-";first=0}
+         }
+         if (x.target.textContent == "+") { result=result+parseFloat(button[0].textContent);button[0].textContent=result;clear=1;op="+";first=0}
+
+         if (x.target.textContent == "=") {
+            if (op=="+") result=result+parseFloat(button[0].textContent);
+            if (op=="-") result=result+parseFloat(button[0].textContent);
+            button[0].textContent = result;
+            clear=1;
+            op="";
+            first=1;
+        }
+        if (x.target.textContent == "C") {
+            button[0].textContent = "";
+            clear=0;
+            przecinek=0;
+            op="";
+            first=1;
+        }
     }
     var main = document.getElementById("container");
     tab.forEach((el,id) => {
         var item=document.createElement("div");
-        if (el=="Display") item.className="resultDisplay"
+        if (el==="") item.className="resultDisplay"
         else if (el==0) item.className="buttonBig";
         else item.className="buttonSmall";
         item.textContent=el;
-        item.addEventListener("click", getData);
+        item.addEventListener("click", x => calculator(x));
         main.appendChild(item);
     });
 }
